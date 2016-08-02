b3ff2e85898fd61e319a69ee8e88f057a5987a6b
samanehash <ashoori.samaneh@gmail.com>
2016-08-02 16:35:33 +0200

    changes on filterReferenceForFlo.py script

--- filterReferenceForFlo.py
+++ filterReferenceForFlo.py
@@ -17,9 +17,9 @@ sprotRecList_batch2 = []
 
 
 ###sprot reference filtering
-SprotRef_batch1 = "/home/samaneh/AHRD/data/reference/non_red_sprot_batch_1_references.fasta"
-SprotRef_batch2 = "/home/samaneh/AHRD/data/reference/non_red_sprot_batch_2_references.fasta"
-des_blacklist = open("/home/samaneh/AHRD/data/blacklists/blacklist_descline_sprot.txt", "r")
+###SprotRef_batch1 = "/home/samaneh/AHRD/data/reference/non_red_tair10_batch_1_references.fasta"
+SprotRef_batch2 = "/home/samaneh/AHRD/data/reference/non_red_tair10_batch_2_references.fasta"
+des_blacklist = open("/home/samaneh/AHRD/data/blacklists/blacklist_descline.txt", "r")
 blacklist_sprot = des_blacklist.readlines()
 
 for token in blacklist_sprot:
@@ -27,11 +27,11 @@ for token in blacklist_sprot:
 
 
 #sprot_batch1 filtering
-for record in SeqIO.parse(SprotRef_batch1, "fasta"):
-	for token in desList_sprot:
-		if token.lower() in record.description.lower():
-			if record.id not in sprotRecList_batch1:
-				sprotRecList_batch1.append(record.id) # save the sequence id which CONTAINS blacklist tokens
+###for record in SeqIO.parse(SprotRef_batch1, "fasta"):
+###	for token in desList_sprot:
+###		if token.lower() in record.description.lower():
+###			if record.id not in sprotRecList_batch1:
+###				sprotRecList_batch1.append(record.id) # save the sequence id which CONTAINS blacklist tokens
 
 
 #sprot_batch2 filtering
@@ -45,17 +45,17 @@ for record in SeqIO.parse(SprotRef_batch2, "fasta"):
 #############################################################################################
 #### generate two new reference sets for each set ####
 
-sprotRefFiltered_batch1 = open("/home/samaneh/AHRD/data/reference/sprot_batch_1_reference_filtered.fasta","w")	
-sprotRefincBlacklist_batch1 = open("/home/samaneh/AHRD/data/reference/sprot_batch_1_reference_incBlacklist.fasta","w") # CONTAINS blacklist tokens
+###sprotRefFiltered_batch1 = open("/home/samaneh/AHRD/data/reference/TAIR10_1_reference_filtered.fasta","w")	
+###sprotRefincBlacklist_batch1 = open("/home/samaneh/AHRD/data/reference/TAIR10_1_reference_incBlacklist.fasta","w") # CONTAINS blacklist tokens
 
-sprotRefFiltered_batch2 = open("/home/samaneh/AHRD/data/reference/sprot_batch_2_reference_filtered.fasta","w")	
-sprotRefincBlacklist_batch2 = open("/home/samaneh/AHRD/data/reference/sprot_batch_2_reference_incBlacklist.fasta","w") # CONTAINS blacklist tokens
+sprotRefFiltered_batch2 = open("/home/samaneh/AHRD/data/reference/TAIR10_2_reference_filtered.fasta","w")	
+sprotRefincBlacklist_batch2 = open("/home/samaneh/AHRD/data/reference/TAIR10_2_reference_incBlacklist.fasta","w") # CONTAINS blacklist tokens
 
-for record in SeqIO.parse(SprotRef_batch1, "fasta"):
-	if record.id in sprotRecList_batch1:		
-		SeqIO.write(record, sprotRefincBlacklist_batch1, "fasta")
-	else:	
- 		SeqIO.write(record, sprotRefFiltered_batch1, "fasta")
+###for record in SeqIO.parse(SprotRef_batch1, "fasta"):
+###	if record.id in sprotRecList_batch1:		
+###		SeqIO.write(record, sprotRefincBlacklist_batch1, "fasta")
+###	else:	
+### 		SeqIO.write(record, sprotRefFiltered_batch1, "fasta")
  		#if record.id not in sprotFilteredList_batch1:
  		#	sprotFilteredList_batch1.append(record.id)
 
@@ -67,8 +67,8 @@ for record in SeqIO.parse(SprotRef_batch2, "fasta"):
  		#if record.id not in sprotFilteredList:
  		#	tairFilteredList.append(record.id)
 
-sprotRefFiltered_batch1.close()
-sprotRefincBlacklist_batch1.close()
+###sprotRefFiltered_batch1.close()
+###sprotRefincBlacklist_batch1.close()
 sprotRefFiltered_batch2.close()
 sprotRefincBlacklist_batch2.close()
 
