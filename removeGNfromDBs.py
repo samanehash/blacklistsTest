4c2092c858411604b8ba56ccc0547ceebe5b48bd
samanehash <ashoori.samaneh@gmail.com>
2016-08-11 19:28:13 +0200

    removeGNfromDBs is updated

--- removeGNfromDBs.py
+++ removeGNfromDBs.py
@@ -5,32 +5,36 @@
 from Bio import SeqIO
 import pandas as pd
 import numpy as np
+from pandas import ExcelWriter
 
 sprotDB = "/home/samaneh/AHRD/data/db/uniprot_sprot.fasta"
 ahrdResult = pd.read_excel(pd.ExcelFile("/home/samaneh/AHRD/outputs/xlsx/test_evaluator_output_both_3.xlsx"))
 
-
-##colnames= = ["AHRD Score","Tair Score","SwissProt Score","Trembl Score"]
-
-####extract the columns names as they are placed in 3rd row
 dfColnames = ahrdResult.ix[2]
 rownames = pd.Series(range(1,len(ahrdResult)-2))
 ahrdResult = ahrdResult.rename(columns=dfColnames).drop(ahrdResult.index[:3])
 ahrdResult.index = rownames
-ahrdResult = ahrdResult["Human-Readable-Description"] #filter columns except evaluation scores
-##ahrdResult.columns = colnames
-#ahrdResult = ahrdResult.astype(float)
+ahrdResult = ahrdResult["Human-Readable-Description"]
 
+pattern = "GN="
+geneList = []
 
+geneNames = open("/home/samaneh/AHRD/outputs/sprotGeneNames.txt","w")
 
-#tairFiltered = open("/home/samaneh/AHRD/data/db/TAIR10_pep_20101214_updated_batch_2_removed.fasta", "w")
 for dbSeq in SeqIO.parse(sprotDB, "fasta"):
-	#flag = False
-	for refDB in ahrdResult:
-		if dbSeq.id == refDB :
-			print dbSeq
-			#flag = True
-	#if flag == False:			
-	#	print 
+	for token in dbSeq.description.split(" "):
+		if pattern in token:
+			geneName = token.strip(pattern)
+			if geneName not in geneList:
+				geneList.append(geneName)
+				geneNames.write("\t")
+				geneNames.write(geneName)
 
-#tairFiltered.close()
+geneNames.close()
+
+print "gene name list's length=", len(geneList)
+
+#for record in ahrdResult:
+#	for name in geneList:
+#		if name in record:
+#			print record
