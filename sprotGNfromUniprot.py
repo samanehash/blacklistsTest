ebd5ca52b0fb832f9f598f69c12da59735bf9271
samanehash <ashoori.samaneh@gmail.com>
2016-09-05 14:47:13 +0200

    a script is updated

--- sprotGNfromUniprot.py
+++ sprotGNfromUniprot.py
@@ -6,20 +6,12 @@ from Bio import SeqIO
 import pandas as pd
 import numpy as np
 import xlsxwriter
-##########################################################################
-
-ahrdResult = pd.read_excel(pd.ExcelFile("/home/samaneh/AHRD/outputs/xlsx/test_evaluator_output_both_sprot3_newTokenBlacklist.xlsx"))
-dfColnames = ahrdResult.ix[2]
-rownames = pd.Series(range(1,len(ahrdResult)-2))
-ahrdResult = ahrdResult.rename(columns=dfColnames).drop(ahrdResult.index[:3])
-ahrdResult.index = rownames
-ahrdResult = ahrdResult["Human-Readable-Description"]
-
 
 ###extract list of geneNames from text file derived from uniprot
 writer2 = xlsxwriter.Workbook("/home/samaneh/AHRD/outputs/AllSprotGeneNames.xlsx")
-worksheet2 = writer2.add_worksheet()
 row2 = 0
+
+tokenFile = open("/home/samaneh/AHRD/outputs/TokenBlacklistIncGNs.txt","w") # create a new token blacklist including all swissprot gene names
 
 geneNames = []
 file = open("/home/samaneh/AHRD/data/uniprot-all.tab","r")
@@ -30,4 +22,6 @@ for line in file.readlines():
 			geneNames.append(gn)
 			worksheet2.write(row2,0,gn)
 			row2 = row2 + 1
+			tokenFile.write("^" + gn + "$" + "\n") # add unrepeated gene names into the new token blacklist
 
+tokenFile.close()
