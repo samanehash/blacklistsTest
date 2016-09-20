59d2a1722d62bd54b4c1a42ba025aad22bd7fa68
samanehash <ashoori.samaneh@gmail.com>
2016-09-20 13:48:02 +0200

    changed

--- extractClustersDescriptions.py
+++ extractClustersDescriptions.py
@@ -10,37 +10,54 @@ import re
 
 ##################################################################################
 
-clstrFile = open("/home/samaneh/AHRD/clustering/uniprot_sprot_clusterd.clstr","r")
+clstrFile = open("/home/samaneh/AHRD/clustering/uniprot_sprot_clusterd_70percents.clstr","r")
 clstr = clstrFile.readlines()
 seqIdDict = dict()
 seqIdList = []
 clstrNumber = "Null"
+#tempWriter = xlsxwriter.Workbook("/home/samaneh/AHRD/clustering/clusters.xlsx")
+#tempsheet = tempWriter.add_worksheet()
+temprow = 0
+tempFile = open("/home/samaneh/AHRD/clustering/clusters_70.txt","w")
 
 for line in clstr:
 
 	if ">Cluster" in line:
 		if clstrNumber != "Null":
 			seqIdDict[clstrNumber]=seqIdList
+			#tempsheet.write(temprow,0,clstrNumber)
+			#print clstrNumber
+			l = len(seqIdList)
+			#for i in range(1,l+1):
+			#	tempsheet.write(temprow,i,seqIdList[i-1])	
+			#temprow = temprow + 1
+			#print seqIdList, "\n"
+			for i in range (0,l):
+				tempFile.write(seqIdList[i])
+				tempFile.write(" ")
+			tempFile.write("\n")
 			seqIdList = [] 
 		clstrNumber = re.sub("\>[A-Za-z]*\s","",line) 
 
 	if ">sp|" in line:
 		line = re.sub(".*>sp\|","",line)
 		seqId = re.sub("\|.*","",line)
-		seqIdList.append(seqId)
+		seqId = seqId.strip("\n")
+		seqIdList.append(seqId)	
 
-tremblInFile = "/home/samaneh/AHRD/data/db/uniprot_sprot.fasta"
-row = 0
-writer = xlsxwriter.Workbook("/home/samaneh/AHRD/outputs/clustersDescriptions.xlsx")
-worksheet = writer.add_worksheet()
 
-clusterList = []
+#sprotFile = "/home/samaneh/AHRD/data/db/uniprot_sprot.fasta"
+#row = 0
+#writer = xlsxwriter.Workbook("/home/samaneh/AHRD/outputs/clustersDescriptions.xlsx")
+#worksheet = writer.add_worksheet()
 
-for record in SeqIO.parse(tremblInFile, "fasta"):
-	for key in seqIdDict.keys():
-		for name in seqIdDict[key]:
-			if name in record.name:
-				print record.description
-				worksheet.write(row,0,key)
-				worksheet.write(row,1,record.description)
-				row = row + 1
+#clusterList = []
+
+#for record in SeqIO.parse(sprotFile, "fasta"):
+#	for key in seqIdDict.keys():
+#		for name in seqIdDict[key]:
+#			if name in record.name:
+#				print record.description
+#				worksheet.write(row,0,key)
+#				worksheet.write(row,1,record.description)
+#				row = row + 1
