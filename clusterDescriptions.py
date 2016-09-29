4317b98ce577ea611a4ef53aea486d77fb268df0
samanehash <ashoori.samaneh@gmail.com>
2016-09-29 12:03:35 +0200

    updated

--- clusterDescriptions.py
+++ clusterDescriptions.py
@@ -11,31 +11,30 @@ import numpy as np
 import xlsxwriter
 import re
 import multiprocessing
+from multiprocessing.dummy import Pool
+import time
 
 ###########################################
 
-### generate a list whose each item is a list of all names in a cluster>2 
-def prepareNamesList(clusterNamesFile):  
+### generate a list whose each item is a list of all names in a cluster>1
+def prepareName(clusterFile):
 
-	idList = []
-	lines = clusterNamesFile.readlines()
+	lines = clusterFile.readlines()
+
+	namesList = []
 	for line in lines:
 		words = line.split()
-		idList.append(words)
+		if len(words)>1:
+			namesList.append(words)
 
-	namesList = [] 
-	for item in idList:
-		if len(item)>2:
-			namesList.append(item)
-
-	return namesList		
+	return namesList
 
 ### extract the descriptions related to each name of cluster from sprot database file
-def writingNames(nList, indx):
-
+def writingNames(namesList, indx):
 	selDesList = []
-	sprotFile = "/home/samaneh/AHRD/data/db/uniprot_sprot.fasta"
-	for name in nList[indx]:
+	sprotFile = "/home/samaneh/AHRD/data/db/uniprot_sprot_filteredForClustering.fasta"
+	for name in namesList[indx]:
+		#print name
 		selDes = ""
 		selDes += name
 		selDes += "\t"
@@ -45,32 +44,29 @@ def writingNames(nList, indx):
 				des = re.sub("OS=.*","",des)
 				selDes += des
 				selDes += "\n"		
-		selDesList.append(selDes)		
+		selDesList.append(selDes)
 	return selDesList	
 
 def handler():
 
 ### call prepareNamesList function
 	clusterFile = open("/home/samaneh/AHRD/clustering/clusters.txt","r")
-	namesList = prepareNamesList(clusterFile)
+	namesList = prepareName(clusterFile)
 
+#p = Pool(4)
+### to check CPU band you can use: multiprocessing.cpu_count()
+### result here shows 4 then it also make sense to use pool(4)
+### however there is no limitation for pool and the limitation 
+
+### will be put on by CPU itself
 	rng = range(0, len(namesList))
-	p = multiprocessing.Pool(4)
 
-	### to check CPU band you can use: multiprocessing.cpu_count()
-	### result here shows 4 then it also make sense to use pool(4)
-	### however there is no limitation for pool and the limitation 
-	### will be put on by CPU itself
-
-### call writingNames dunction and write the extracted descriptions into a file
+### call writingNames dunction and write the extracted descriptions into a file	
 	with open("/home/samaneh/AHRD/clustering/clusteredDescriptions_multiprocessed.txt","w") as f:
 		for result in (writingNames(namesList, i) for i in rng):
 			for n in result: 
 				f.write(n)
 			f.write("\n############\n")
 
-
-if __name__ == "__main__": 		
-	handler()						 
-
-
+if __name__ == "__main__":
+	handler()
