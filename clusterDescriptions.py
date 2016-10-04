38f65964decebca25843de36597df6deb2bc0a2e
samanehash <ashoori.samaneh@gmail.com>
2016-10-04 10:11:35 +0200

    updated

--- clusterDescriptions.py
+++ clusterDescriptions.py
@@ -16,57 +16,52 @@ import time
 
 ###########################################
 
-### generate a list whose each item is a list of all names in a cluster>1
-def prepareName(clusterFile):
-
-	lines = clusterFile.readlines()
+class cluster():
 
 	namesList = []
-	for line in lines:
-		words = line.split()
-		if len(words)>1:
-			namesList.append(words)
+	selDesList = []
 
-	return namesList
+### generate a list whose each item is a list of all names in a cluster>1
+	def prepareName(self, clusterFile):
+
+		lines = clusterFile.readlines()
+		for line in lines:
+			words = line.split()
+			if len(words)>1:
+				self.namesList.append(words)
 
 ### extract the descriptions related to each name of cluster from sprot database file
-def writingNames(namesList, indx):
-	selDesList = []
-	sprotFile = "/home/samaneh/AHRD/data/db/uniprot_sprot_filteredForClustering.fasta"
-	for name in namesList[indx]:
-		#print name
-		selDes = ""
-		selDes += name
-		selDes += "\t"
-		for record in SeqIO.parse(sprotFile, "fasta"):
-			if name in record.name:
-				des = record.description.strip(record.name).strip(" ")
-				des = re.sub("OS=.*","",des)
-				selDes += des
-				selDes += "\n"		
-		selDesList.append(selDes)
-	return selDesList	
+	def writingNames(self,idnx):
+		self.selDesList = []
+		sprotFile = "/home/samaneh/AHRD/data/db/uniprot_sprot_filteredForClustering.fasta"
+
+		for name in self.namesList[idnx]:
+			selDes = ""
+			selDes += name
+			selDes += " "
+			selDes += "| "
+			for record in SeqIO.parse(sprotFile, "fasta"):
+				if name in record.name:
+					des = record.description.strip(record.name).strip(" ")
+					des = re.sub("OS=.*","",des)
+					selDes += des
+					selDes += "\n"	
+
+			self.selDesList.append(selDes)	
 
 def handler():
-
-### call prepareNamesList function
+	n = cluster()
 	clusterFile = open("/home/samaneh/AHRD/clustering/clusters.txt","r")
-	namesList = prepareName(clusterFile)
+	n.prepareName(clusterFile)
+	with open("/home/samaneh/AHRD/clustering/clusteredDescriptions_final.txt","w") as f:
+		for i in range(0, len(n.namesList)):
+			n.writingNames(i)
+			for item in n.selDesList:
+				f.write(item)
+			f.write("\n############\n")		
 
-#p = Pool(4)
-### to check CPU band you can use: multiprocessing.cpu_count()
-### result here shows 4 then it also make sense to use pool(4)
-### however there is no limitation for pool and the limitation 
 
-### will be put on by CPU itself
-	rng = range(0, len(namesList))
-
-### call writingNames dunction and write the extracted descriptions into a file	
-	with open("/home/samaneh/AHRD/clustering/clusteredDescriptions_multiprocessed.txt","w") as f:
-		for result in (writingNames(namesList, i) for i in rng):
-			for n in result: 
-				f.write(n)
-			f.write("\n############\n")
 
 if __name__ == "__main__":
+	
 	handler()
