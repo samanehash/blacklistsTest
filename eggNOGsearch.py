6c1b85144638eb73184d13b80697d7712c25d50c
samanehash <ashoori.samaneh@gmail.com>
2016-11-04 13:02:54 +0100

    submission

---
+++ eggNOGsearch.py
@@ -0,0 +1,34 @@
+'''
+@auther: Samaneh
+
+WORKING VERSION
+
+'''
+from Bio import SeqIO
+import pandas as pd
+import numpy as np
+import xlsxwriter
+import re
+
+######################################################
+
+def handler():
+	
+
+
+
+
+
+
+
+
+
+
+
+
+if __name__ == "main":
+	handler()
+
+
+
+
