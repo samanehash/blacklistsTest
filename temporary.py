ecf0f906257a5cf0035e51773fa8ae6de056beec
samanehash <ashoori.samaneh@gmail.com>
2016-11-29 14:34:34 +0100

    updated

--- temporary.py
+++ temporary.py
@@ -13,27 +13,34 @@ import xlsxwriter
 import re
 import csv
 import os
+import json
 
 #########################################################################
 
 uniprotIdsFile = open("/home/samaneh/eggNOG/data/other/uniprot-all.tsv", "r")
 uniprotDict = dict()
-
+tempList = []
+newList = []
 ##@sam## extract a dictionary from cluster names as keys and uniprot ids as values
 
-for line in uniprotIdsFile.readlines():
-	clusterId = line.split()[1].split(",")
-	uniprotId = list(line.split()[0])
-	for c in clusterId:               
-		if c in uniprotDict.keys():
-			tempList = uniprotDict[c]
-			tempList.append(uniprotId)
-			uniprotDict.update({c:tempList})
-		else:	
-			uniprotDict.update({c:uniprotId})
 
-uniprotDicFile = open("/home/samaneh/essNOG/uniprotIdsDictionary.json","w")
-uniprotDicFile.write(uniprotDict)
+for line in uniprotIdsFile.readlines():
+	clusterName = line.split()[0]
+	#clusterNamesList.append(line.split()[0])
+	uniprotId = line.split()[1].split(",") 
+	if "-" in uniprotId:
+		uniprotId.remove("-")
+	if clusterName in uniprotDict.keys():
+		tempList = uniprotDict[clusterName]
+		newList = tempList + uniprotId
+		uniprotDict.update({clusterName:newList})
+	else:	
+		uniprotDict.update({clusterName:uniprotId})
+print uniprotDict
+with open("/home/samaneh/eggNOG/uniprotIdsDictionary.json","w") as uniprotDicFile:
+	json.dump(uniprotDict, uniprotDicFile)
+
+uniprotDicFile.close()
 print "done"
 ##################################
 
@@ -47,28 +54,26 @@ uniprotIdsList = []
 sprotOutFile = open("/home/samaneh/eggNOG/sprotIncludedInEggnog.fasta","w")
 descList = []
 
-for k in uniprotDict.keys():	##@sam## in each cluster name search all uniprot ids in sprot database, if it is found 
-	newValueList = []
-	for record in SeqIO.parse(sprotDbFile,"fasta"):
-		sprotId = record.id.split("|")[1]
-		for u in uniprotDict[k]:
-			if u == sprotId:
-				newValueList.append(record.id)
-				des = record.description.strip(record.name).strip(" ")
-				des = re.sub("OS=.*","",des)	
-				descList.append(des)
-	uniprotDict.update({k:newValueList}) 	##@sam## updates the dictionary in a way that includes only uniprot id that are included in sprot
 with open("/home/samaneh/eggNOG/clusteredDescriptions.txt","w") as f:
-	for item in descList:
-		f.write(item)
-	f.write("\n############\n")		
+	for cluster in uniprotDict.keys():	##@sam## in each cluster name search all uniprot ids in sprot database, if it is found 
+		newValueList = []
+		for record in SeqIO.parse(sprotDbFile,"fasta"):
+			sprotId = record.id.split("|")[1]
+			for uid in uniprotDict[cluster]:
+				if uid == sprotId:
+					sprotIdList.append(record.id)
+					des = record.description.strip(record.name).strip(" ")
+					des = re.sub("OS=.*","",des)	
+					descList.append(des)
+		
+		f.write("\n")
+		f.write(cluster)
+		for i in range(0,len(sprotIdsList)):
+			f.write(sprotIdsList[i])
+			f.write(" | ")
+			f.write(descList[i])
+		f.write("\n###############\n")
 
-
-
-
-print uniprotDict
-
-##@sam## create a descriptions file like what clusterDescriptions.py creates
 
 
 
