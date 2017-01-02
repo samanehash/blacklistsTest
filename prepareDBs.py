69f90c994631c968386c93a0263e32b190038601
samanehash <ashoori.samaneh@gmail.com>
2017-01-02 17:52:20 +0100

    changed to eggNOG blast db prepration

--- prepareDBs.py
+++ prepareDBs.py
@@ -1,24 +1,26 @@
 '''
 @auther: Samaneh
 
+Preparing the database for BLAST search by creatig a new fasta file 
+out of the uniprot version of database excluding those sequences 
+which are included in reference file
+
 '''
 from Bio import SeqIO
 
 
-#sprotInFile = "/home/samaneh/AHRD/data/db/uniprot_sprot.fasta"
-tremblInFile = "/home/samaneh/AHRD/data/db/uniprot_trembl.fasta"
-os = "OS=Blumeria"
-desList = []
+sprotInFile = open("/home/samaneh/AHRD/data/db/uniprot_sprot.fasta","r")
+referenceFile = open("/home/samaneh/eggNOG/output/clstrd_ref_myAlgo_descs.fasta","r")
+excludingIds = []
 
-#sprotOutFile = open("/home/samaneh/AHRD/data/db/uniprot_sprot_BG_removed.fasta", "w")
-#for record in SeqIO.parse(sprotInFile, "fasta"):
-#	if os not in record.description:
-#		SeqIO.write(record, sprotOutFile, "fasta")
-#sprotOutFile.close()
+for seq in SeqIO.parse(referenceFile,"fasta") :
+	excludingIds.append(seq.id.split("|")[1])
 
-tremblOutFile = open("/home/samaneh/AHRD/data/db/uniprot_trembl_BG_removed.fasta", "w")
-for record in SeqIO.parse(tremblInFile, "fasta"):
-	if os not in record.description:
-		SeqIO.write(record, tremblOutFile, "fasta")
-tremblOutFile.close()
+sprotOutFile = open("/home/samaneh/AHRD/data/db/uniprot_sprot_clstrd_myAlgo_removed.fasta", "w")
+for record in SeqIO.parse(sprotInFile, "fasta"):
+	recordId = record.id.split("|")[1]
+	if recordId not in excludingIds:
+		SeqIO.write(record, sprotOutFile, "fasta")
+sprotOutFile.close()
+
 
